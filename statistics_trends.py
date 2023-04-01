a88ab4a6626791d3c94e7316a6ca6694b93dcfa8
Beni1927 <126979476+Beni1927@users.noreply.github.com>
2023-04-01 14:59:33 +0530

    Bar plot

--- statistics_trends.py
+++ statistics_trends.py
@@ -8,9 +8,12 @@ Created on Wed Mar 29 15:38:14 2023
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 d1 = "./D1.csv"
 d2 = "./D2.csv"
+d5 = "./D5.csv"
+heatmap = "./heat_china"
 
 def data(fn):
     d = pd.read_csv(fn)
@@ -19,6 +22,14 @@ def data(fn):
     dt = pd.DataFrame.transpose(dfn)
     return(dfn, dt)
 
-df, dt = data(d1)
+df, dt = data(d5)
 print(dt)
+
+df.plot(x = "Country Name", y=["1990 [YR1990]", "2000 [YR2000]", "2012 [YR2012]", "2013 [YR2013]", "2014 [YR2014]", "2015 [YR2015]"], kind= "bar", figsize = (9,8))
+
+df1, dt1 = data(heatmap)
+corr_mat = df1.corr()
+
+            
+
 
